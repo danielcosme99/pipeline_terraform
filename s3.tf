b77bea6c7b715bbff237641c1619efab9391a03d
danielcosme99 <danielcosme99@gmail.com>
2025-08-13 00:33:50 -0500

    prueba terraform 4

--- s3.tf
+++ s3.tf
@@ -2,6 +2,10 @@ resource "aws_s3_bucket" "my_bucket" {
   bucket = "test-2-jeremy-terraform"
 }
 
+resource "aws_s3_bucket" "my_bucket_1" {
+  bucket = "test-3-cosme-terraform"
+}
+
 terraform {
   backend "s3" {
     bucket         = "pipeline-terraform-state-s3"
